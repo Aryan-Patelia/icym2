5eb359886c2bd2a52898bb8c16c3a627e9cca910
Aryan-Patelia <145205741+Aryan-Patelia@users.noreply.github.com>
2024-05-29 12:14:58 +0530

    tem

--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div class=" footer ">
-    <h6>ALl rights reserved by ICYM Ahmedabad Diocese &copy 2024</h6>
-</div>
--- posts/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    require_once('functions.php');
-
-    $posts = new posts;
-
-?>
